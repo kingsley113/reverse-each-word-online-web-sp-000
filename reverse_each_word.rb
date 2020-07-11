a93682c2654ea389ef3f0b04b07ff456900c6b07
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-11 04:10:48 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(word)
+  word_array = word.split(",")
+  word_array.each do | word |
+    reversed_words << word.reverse
+  end
+  reversed_phrase = reversed_words.join
+end
+
+  
+  
