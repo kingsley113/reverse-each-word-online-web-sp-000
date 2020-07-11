e4ae34118b92876e9e2ac9055f350dc424a8f51c
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-11 04:16:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(word)
-  word_array = word.split(" ")
-  reversed_words = []
-  word_array.each do | word |
-    reversed_words << word.reverse
-  end
-  reversed_phrase = reversed_words.join(" ")
-end
+#def reverse_each_word(word)
+#  word_array = word.split(" ")
+#  reversed_words = []
+#  word_array.each do | word |
+#    reversed_words << word.reverse
+#  end
+#  reversed_phrase = reversed_words.join(" ")
+#end
 
 
 def reverse_each_word(word)
