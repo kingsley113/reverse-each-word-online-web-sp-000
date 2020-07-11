6aef86122d56597da97a228e1f14493f138d8ddf
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-11 05:06:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,14 +11,17 @@
 def reverse_each_word(word)
   word_array = word.split(" ")
   reversed_words = []
-  word_array.collect do |word|
-     reversed << word.reverse
-     puts "#{reversed}"
-  end
-  reversed_phrase = word_array.join(" ")
+  reversed = word_reverse(word_array)
+
+  reversed_phrase = reversed.join(" ")
   puts "#{reversed_phrase}"
 end
 
 
 test_sentence = "This is the test sentence, why wont you reverse!?"
 reverse_each_word(test_sentence)
+
+
+def word_reverse(word)
+  word.collect {|x| x.reverse}
+end
